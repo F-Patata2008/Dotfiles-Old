f241d9e106131ebdd8b2a550ef51ff68653b829c
Felipe Colli <felipe.colli.scratch@gmail.com>
2025-07-04 19:08:57 -0400

    Auto-commit: 2025-07-04 19:08:56

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -1,9 +1,35 @@
-return {
+return { 
     {
-        "lervag/vimtex",
-        lazy = false,     -- we don't want to lazy load VimTeX
-        init = function()
-            vim.g.vimtex_view_method = "zathura"
-        end
+    "lervag/vimtex",
+    -- Carga el plugin solo cuando abres un archivo .tex, .latex o .bib. Es más eficiente.
+    ft = { "tex", "latex", "bib" },
+    init = function()
+      -- Opciones globales que DEBEN establecerse ANTES de que el plugin se cargue.
+      
+      -- ¡¡ESTA ES LA LÍNEA CLAVE QUE RESUELVE TU PROBLEMA DE BIBLIOGRAFÍA!!
+      -- Le dice a vimtex que use latexmk, que a su vez ejecuta Biber por ti.
+      vim.g.vimtex_compiler_method = "latexmk"
+
+            
+      -- ¡¡ESTA ES LA CONFIGURACIÓN CLAVE PARA HYPRLAND/WAYLAND!!
+      -- En lugar de solo el nombre, le damos una tabla de configuración.
+      vim.g.vimtex_view_zathura = {
+        -- Activa el modo Wayland, que usa DBus en lugar de X11 Window IDs.
+        wayland = 1,
+        -- Ayuda a nvr a encontrar la instancia correcta de Neovim
+        nvim_instance = vim.v.servername,
+      }
+
+      -- Elige tu visor de PDF. Asegúrate de tener zathura instalado.
+      vim.g.vimtex_view_method = "zathura"
+
+      -- Habilita el corrector ortográfico para comentarios en LaTeX
+      vim.g.vimtex_spell_enabled = 1
+    end,
+    config = function()
+      -- Aquí van tus atajos de teclado para vimtex.
+      local map = vim.keymap.set
+      local opts = { silent = true, noremap = true }
+    end,
     }
 }
