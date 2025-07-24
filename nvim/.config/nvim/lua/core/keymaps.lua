4a8560ff1423401d455fd4c155e01af3ad9b9e90
Felipe Colli <felipe.colli.scratch@gmail.com>
2025-07-24 12:00:47 -0400

    Auto-commit: 2025-07-24 12:00:46

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,5 +1,6 @@
-vim.keymap.set('n', '<C-s>', ':w<CR>', {})
-vim.keymap.set('n', '<C-q>', ':q<CR>', {})
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { noremap = true, silent = true, desc = "Save file" })
+vim.keymap.set("n", "<C-q>", "<cmd>q<CR>", { noremap = true, silent = true, desc = "Quit file" })
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc = "Select all" })
 
 
 return {
